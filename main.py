e6007806dc54dc5435d3bba75c933d6ea26bf1ce
AzmatG <azmat.ginowrie@laetottenham.ac.uk>
2024-11-22 12:13:37 +0000

    finished

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ class Person:
         self.name = name
         self.age = age
         self.gender = gender
+        self.mentor = ""
     def set_details(self, name, age, gender):
         self.name = name
         self.age = age
         self.gender = gender
     def get_details(self):
         return f"Name: {self.name}\nAge: {self.age}\n Gender: {self.gender}"
-        
+    def get_mentor(self):
+        try:
+            return f"{self.mentor} is mentoring {self.name}"
+        except len(self.mentor) == 0:
+            return "No mentor assigned."
 class Student(Person):
     def __init__(self, name, age, gender, student_id, course):
         super().__init__(name, age, gender)
@@ -23,7 +28,7 @@ class Student(Person):
         self.grade.append(grade)
     def calculate_average_grade(self):
         try: 
-            return sum(self.grade)/len(self.grade)
+            return round(sum(self.grade)/len(self.grade), 2)
         except:
             return "an exception has occurred."
     def get_student_summary(self):            
@@ -34,16 +39,26 @@ class Professor(Person):
         self.staff_id = staff_id
         self.department = department
         self.salary = salary
+        self.mentors = []
     def set_professor_details(self, staff_id, department, salary):
         self.staff_id = staff_id
         self.department = department
         self.salary = salary
     def give_feedback(self, student, feedback):
-        print(f"Feedback for {student}: {feedback}")
+        return f"Feedback for {student.name}: {feedback}"
     def increase_salary(self, percentage):
-        self.salary += self.salary(percentage/100)
+        self.salary += self.salary*(percentage/100)
     def get_professor_summary(self):
-        return f"Name: {self.name}\nAge: {self.age}\nGender: {self.gender}\nStaff Id: {self.staff_id}\nDepartment: {self.department}\nSalary: {self.salary}"
+        return f"Name: {self.name}\nAge: {self.age}\nGender: {self.gender}\nStaff Id: {self.staff_id}\nDepartment: {self.department} department\nSalary: {self.salary}"
+    def mentor_student(self, student):
+        self.mentors.append(student.name)
+        student.mentor = self.name
+        return f"{self.name} is now mentoring {student.name} on {student.course}."
+    def get_mentored_students(self):
+        try:
+            return f"{self.name} is mentoring {self.mentors}"
+        except len(self.mentors) == 0:
+            return f"{self.name} is mentoring no one"
 class Admin(Person):
     def __init__(self, name, age, gender, admin_id, office, years_of_service):
         super().__init__(name, age, gender)
@@ -53,15 +68,44 @@ class Admin(Person):
     def set_admin_details(self, admin_id, office, years_of_service):
         self.admin_id = admin_id
         self.office = office
-        self.years_of_service
+        self.years_of_service = years_of_service
+    def increment_service_years(self):
+        self.years_of_service += 1
+    def get_admin_summary(self):
+        return f"Name: {self.name} \nAge:{self.age} \nGender: {self.gender} \nAdmin_Id: {self.admin_id}   \nOffice: {self.office} \nYears of Service: {self.years_of_service}"
+        
         
  
  
- 
+ #A Showcase of functionality
  
  
     
     
 student = Student("Kana", 18, "Female", "K18", "Acting")
-professor = Professor("Aizen", 90, "Block", "12345", "Spanish", 0)
-#professor.give_feedback(student.name, "Nice slap!")
+professor = Professor("Aizen", 90, "Block", "12345", "Spanish", 100)
+admin = Admin("Himmel", 65, "Male", "H65", "Room 12F", 23)
+print("\nAdding grades\n")
+print(student.get_student_summary())
+student.add_grade(90)
+student.add_grade(10)
+student.add_grade(40)
+print(student.get_student_summary())
+print(f"Average grade => {student.calculate_average_grade()}")
+print("\nProfessor Things\n")
+
+print(professor.give_feedback(student, "Good Work!"))
+print(f"Old Salary => {professor.salary}")
+professor.increase_salary(10)
+print(f"New Salary => {professor.salary}")
+
+print("\nAdmin Things\n")
+print(f"Old years => {admin.years_of_service}")
+admin.increment_service_years()
+print(f"New Years => {admin.years_of_service}")
+
+print("\nMentoring System\n")
+
+print(professor.mentor_student(student))
+print(student.get_mentor())
+print(professor.get_mentored_students())
